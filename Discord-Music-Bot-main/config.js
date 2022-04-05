f7a4cb2abe1d9aaf44a7a7936af72e2db52b069f
BLEKSHEEP <98331307+lammin6@users.noreply.github.com>
2022-04-05 20:56:55 +0530

    Update config.js

--- Discord-Music-Bot-main/config.js
+++ Discord-Music-Bot-main/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-        TOKEN: '', //write your discord bot token
+        TOKEN: 'OTYwOTIwMjU3Njg4NjUzODY1.Ykxcug.TNXS9DzGjyfVU8nUEAOdwWck_5U', //write your discord bot token
         px: '!',
         playing: '#Save Ukraine 💛💙',
 
